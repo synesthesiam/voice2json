2693fac1040305659766b5c5503bf2a04179de61
Michael Hansen <michael.hansen.24@us.af.mil>
2019-10-11 16:48:01 -0400

    Added basic web interface

---
+++ web/app.py
@@ -0,0 +1,356 @@
+#!/usr/bin/env python3
+import io
+import json
+import argparse
+import subprocess
+import logging
+import time
+import shlex
+import tempfile
+import threading
+import wave
+from uuid import uuid4
+from pathlib import Path
+from typing import Optional, Dict, Any, Tuple, BinaryIO, List
+
+import pydash
+from flask import Flask, request, render_template, send_from_directory, flash
+
+# -----------------------------------------------------------------------------
+
+profile_path: Optional[Path] = None
+profile: Dict[str, Any] = {}
+
+# Flask application
+app = Flask("voice2json")
+app.secret_key = str(uuid4())
+
+logger = logging.getLogger("app")
+
+# -----------------------------------------------------------------------------
+
+record_proc: Optional[subprocess.Popen] = None
+record_file: Optional[BinaryIO] = None
+recording: bool = False
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    """Handles recording, transcription, and intent recognition."""
+    global record_proc, record_file, recording
+
+    # Sentence to recognize
+    sentence: str = ""
+
+    # Recognized intent
+    intent: Optional[Dict[str, Any]] = None
+
+    # List of (word, entity) tuples from recognized intent
+    words_entities: List[Tuple[str, Dict[str, Any]]] = []
+
+    if request.method == "POST":
+        # WAV audio data
+        wav_data: Optional[bytes] = None
+
+        # Check if start/stopping recording
+        if "record" in request.form:
+            if record_proc is None:
+                # Start recording
+                record_file = tempfile.NamedTemporaryFile(mode="wb+")
+                record_command = shlex.split(
+                    pydash.get(profile, "audio.record-command")
+                )
+                logger.debug(record_command)
+
+                record_proc = subprocess.Popen(record_command, stdout=record_file)
+                recording = True
+            else:
+                # Stop recording
+                record_proc.terminate()
+                record_proc.wait()
+                record_proc = None
+                recording = False
+
+                # Read raw audio data from temp file
+                record_file.seek(0)
+                raw_audio_data = record_file.read()
+
+                # Convert to WAV data
+                with io.BytesIO() as wav_buffer:
+                    with wave.open(wav_buffer, mode="wb") as wav_file:
+                        wav_file.setframerate(16000)
+                        wav_file.setsampwidth(2)
+                        wav_file.setnchannels(1)
+                        wav_file.writeframesraw(raw_audio_data)
+
+                    wav_data = wav_buffer.getvalue()
+
+                # Clean up
+                del record_file
+                record_file = None
+        elif "upload" in request.form:
+            if "wavfile" in request.files:
+                # Get WAV data from file upload
+                wav_file = request.files["wavfile"]
+                wav_data = wav_file.read()
+            else:
+                flash("No WAV file given", "danger")
+        elif "recognize" in request.form:
+            # Get sentence to recognize from form
+            sentence = request.form["sentence"]
+            if len(sentence) == 0:
+                flash("No sentence to recognize", "danger")
+
+        # ---------------------------------------------------------------------
+
+        if wav_data is not None:
+            # Transcribe WAV
+            logger.debug(f"Transcribing {len(wav_data)} byte(s)")
+            transcribe_result = json.load(
+                voice2json("transcribe-wav", text=False, input=wav_data)
+            )
+            sentence = transcribe_result.get(
+                "raw_text", transcribe_result.get("text", "")
+            )
+
+        if len(sentence) > 0:
+            # Recognize text
+            recognize_result = json.load(
+                voice2json("recognize-intent", input=json.dumps(transcribe_result))
+            )
+            intent = recognize_result
+
+        # Process intent
+        if intent is not None:
+            char_index = 0
+
+            # Map from start character index to entity
+            start_to_entity = {
+                e.get("raw_start", -1): e for e in intent.get("entities", [])
+            }
+            entity = None
+
+            # Go through words (tokens)
+            for token in intent.get("raw_tokens", intent.get("tokens", [])):
+                if entity and (char_index >= entity.get("raw_end", -1)):
+                    # Entity has finished
+                    words_entities.append(
+                        (entity.get("raw_value", entity.get("value", "")), entity)
+                    )
+                    entity = None
+
+                if entity is None:
+                    # Entity is starting
+                    entity = start_to_entity.get(char_index)
+
+                if entity is None:
+                    # Regular word
+                    words_entities.append((token, None))
+
+                char_index += len(token) + 1  # +1 for space
+
+            if entity:
+                # Catch entity at end of sentence
+                words_entities.append(
+                    (entity.get("raw_value", entity.get("value", "")), entity)
+                )
+
+    # -------------------------------------------------------------------------
+
+    # JSON for display to user
+    intent_str = json.dumps(intent, indent=4) if intent is not None else ""
+
+    return render_template(
+        "index.html",
+        profile=profile,
+        pydash=pydash,
+        sentence=sentence,
+        intent=intent,
+        intent_str=intent_str,
+        words_entities=words_entities,
+        recording=recording,
+    )
+
+
+# -----------------------------------------------------------------------------
+
+
+@app.route("/sentences", methods=["GET", "POST"])
+def sentences():
+    """Reads/writes sentences.ini. Re-trains when sentences are saved."""
+    sentences_path = Path(pydash.get(profile, "training.sentences-file"))
+
+    if request.method == "POST":
+        # Save sentences
+        sentences_text = request.form["sentences"]
+        sentences_path.write_text(sentences_text)
+        do_retrain()
+    else:
+        # Load sentences
+        sentences_text = sentences_path.read_text()
+
+    return render_template(
+        "sentences.html", profile=profile, pydash=pydash, sentences=sentences_text
+    )
+
+
+# -----------------------------------------------------------------------------
+
+
+@app.route("/words", methods=["GET", "POST"])
+def words():
+    """Speaks words, guesses pronunciations, and reads/writes custom_words.txt.
+    Re-trains when custom words are saved."""
+
+    custom_words_path = Path(pydash.get(profile, "training.custom-words-file"))
+    word = ""
+
+    if request.method == "POST":
+        action = request.form["action"]
+
+        if action == "custom words":
+            # Save custom words
+            custom_words_text = request.form["custom_words"]
+            custom_words_path.write_text(custom_words_text)
+            do_retrain()
+        elif action == "pronounce":
+            # Speak or guess pronunciation
+            word = request.form["word"]
+            is_speak = "speak" in request.form
+            if len(word) > 0:
+                if is_speak:
+                    # Speak pronunciation
+                    result = voice2json("pronounce-word", "--nbest", "1", word)
+                else:
+                    # Get multiple guesses
+                    result = voice2json(
+                        "pronounce-word", "--quiet", "--nbest", "3", word
+                    )
+
+                # Display guess(s)
+                for line in result:
+                    flash(line.strip(), "info")
+            else:
+                flash("No word given", "danger")
+
+    # Load custom words
+    custom_words_text = custom_words_path.read_text()
+
+    return render_template(
+        "words.html",
+        profile=profile,
+        pydash=pydash,
+        custom_words=custom_words_text,
+        word=word,
+    )
+
+
+# -----------------------------------------------------------------------------
+# Static Routes
+# -----------------------------------------------------------------------------
+
+
+@app.route("/css/<path:filename>", methods=["GET"])
+def css(filename):
+    return send_from_directory("css", filename)
+
+
+@app.route("/js/<path:filename>", methods=["GET"])
+def js(filename):
+    return send_from_directory("js", filename)
+
+
+@app.route("/img/<path:filename>", methods=["GET"])
+def img(filename):
+    return send_from_directory("img", filename)
+
+
+@app.errorhandler(Exception)
+def handle_error(err) -> Tuple[str, int]:
+    logger.exception(err)
+    return (str(err), 500)
+
+
+# -----------------------------------------------------------------------------
+# Utility Methods
+# -----------------------------------------------------------------------------
+
+
+def voice2json(*args, text=True, input=None, stderr=None):
+    """Calls voice2json with the given arguments and current profile."""
+    global profile_path
+    command = ["voice2json"]
+
+    if profile_path is not None:
+        # Add profile
+        command.extend(["--profile", str(profile_path)])
+
+    command.extend(list(args))
+    logger.debug(command)
+
+    if text:
+        # Text-based I/O
+        return io.StringIO(
+            subprocess.check_output(
+                command, universal_newlines=True, input=input, stderr=stderr
+            )
+        )
+    else:
+        # Binary I/O
+        return io.BytesIO(subprocess.check_output(command, input=input, stderr=stderr))
+
+
+def do_retrain():
+    """Re-trains voice2json profile and flashes warnings for unknown words."""
+    # Re-train
+    start_time = time.time()
+    result = voice2json("train-profile", stderr=subprocess.STDOUT).read()
+    train_seconds = time.time() - start_time
+    flash(f"Re-trained in {train_seconds:0.2f} second(s)", "success")
+
+    logger.debug(result)
+
+    warn_lines = None
+    for line in result.splitlines():
+        line = line.strip()
+        if line.startswith("-") or line.startswith("."):
+            continue
+
+        if "unknown" in line:
+            warn_lines = []
+            line = line + ":"
+
+        if warn_lines is not None:
+            warn_lines.append(line)
+
+    if warn_lines is not None:
+        flash("\n".join(warn_lines), "warning")
+
+
+# -----------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true", help="Enable DEBUG mode")
+    parser.add_argument(
+        "--port", type=int, default=5000, help="Web server port (default: 5000)"
+    )
+    parser.add_argument(
+        "--host", default="127.0.0.1", help="Web server host (default: 127.0.0.1)"
+    )
+    parser.add_argument("--profile", help="Path to voice2json profile")
+    args = parser.parse_args()
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+
+    logger.debug(args)
+
+    if args.profile is not None:
+        profile_path = Path(args.profile)
+
+    # Get profile as JSON from voice2json
+    profile = json.load(voice2json("print-profile"))
+
+    # Start web server
+    app.run(port=args.port, host=args.host, debug=args.debug)
