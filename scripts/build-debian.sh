c4f95b6b0ea4bc800cc6628cc50f8048d8e010ca
Michael Hansen <hansen.mike@gmail.com>
2020-05-14 09:55:21 -0400

    Working on Debian build for armv6

--- scripts/build-debian.sh
+++ scripts/build-debian.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [[ -z "$1" ]]; then
-    echo "Usage: build-debian dist/ [target] [target]"
+    echo "Usage: build-debian dist/"
     exit 1
 fi
 
@@ -18,47 +18,13 @@ version="$(cat "${src_dir}/VERSION")"
 
 # -----------------------------------------------------------------------------
 
-if [[ -z "$1" ]]; then
-    platforms=('linux/amd64' 'linux/arm/v7' 'linux/arm64')
-else
-    platforms=("$@")
-fi
-
-function join { local IFS="$1"; shift; echo "$*"; }
-platform_str=$(join ',' ${platforms[@]})
+: "${PLATFORMS=linux/amd64,linux/arm/v7,linux/arm64,linux/arm/v6}"
 
 # ------------------------------------------------------------------------------
-
-: "${DOCKER_REGISTRY=docker.io}"
 
 echo "Building..."
 docker buildx build \
        "${src_dir}" \
        -f "${src_dir}/Dockerfile.debian" \
-       "--platform=${platform_str}" \
-       --build-arg "DOCKER_REGISTRY=${DOCKER_REGISTRY}" \
-       --tag "${DOCKER_REGISTRY}/voice2json-debian" \
-       --push
-
-# ------------------------------------------------------------------------------
-
-declare -A platform_to_debian
-platform_to_debian=(['linux/amd64']='amd64' ['linux/arm/v6']='armel' ['linux/arm/v7']='armhf' ['linux/arm64']='arm64')
-
-for platform in "${platforms[@]}"; do
-    echo "Packaging..."
-    debian_arch="${platform_to_debian["${platform}"]}"
-    package_name="voice2json_${version}_${debian_arch}"
-
-    docker pull \
-           --platform "${platform}" \
-           "${DOCKER_REGISTRY}/voice2json-debian"
-
-    docker run \
-           --platform "${platform}" \
-           --rm -i --entrypoint /bin/cat \
-           "${DOCKER_REGISTRY}/voice2json-debian" \
-           "/build/${package_name}.deb" > "${dist_dir}/${package_name}.deb"
-
-    echo "Wrote ${dist_dir}/${package_name}.deb"
-done
+       "--platform=${PLATFORMS}" \
+       --output "type=local,dest=${dist_dir}"
--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -18,4 +18,5 @@ docker buildx build \
         "--platform=${platforms}" \
         --build-arg "DOCKER_REGISTRY=${DOCKER_REGISTRY}" \
         --tag "${DOCKER_REGISTRY}/synesthesiam/voice2json:${version}" \
+        --tag "${DOCKER_REGISTRY}/synesthesiam/voice2json" \
         --push
