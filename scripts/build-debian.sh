d4a22049a8e4400342e992eeb26a9788dd8bcd0c
Michael Hansen <hansen.mike@gmail.com>
2020-06-04 15:22:37 -0400

    Copy deb files in build-debian.sh

--- scripts/build-debian.sh
+++ scripts/build-debian.sh
@@ -22,9 +22,36 @@ version="$(cat "${src_dir}/VERSION")"
 
 # ------------------------------------------------------------------------------
 
+rm -f "${dist_dir}/voice2json_${version}_"*.deb
+
 echo "Building..."
 docker buildx build \
        "${src_dir}" \
        -f "${src_dir}/Dockerfile.debian" \
        "--platform=${PLATFORMS}" \
        --output "type=local,dest=${dist_dir}"
+
+# Manually copy out
+in_amd64="${dist_dir}/linux_amd64/voice2json_${version}_amd64.deb"
+out_amd64="${dist_dir}/voice2json_${version}_amd64.deb"
+if [[ -f "${in_amd64}" ]]; then
+    cp "${in_amd64}" "${out_amd64}"
+fi
+
+in_armhf="${dist_dir}/linux_arm_v7/voice2json_${version}_armhf.deb"
+out_armhf="${dist_dir}/voice2json_${version}_armhf.deb"
+if [[ -f "${in_armhf}" ]]; then
+    cp "${in_armhf}" "${out_armhf}"
+fi
+
+in_arm64="${dist_dir}/linux_arm64/voice2json_${version}_arm64.deb"
+out_arm64="${dist_dir}/voice2json_${version}_arm64.deb"
+if [[ -f "${in_arm64}" ]]; then
+    cp "${in_arm64}" "${out_arm64}"
+fi
+
+in_armel="${dist_dir}/linux_arm_v6/voice2json_${version}_armhf.deb"
+out_armel="${dist_dir}/voice2json_${version}_armel.deb"
+if [[ -f "${in_armel}" ]]; then
+    cp "${in_armel}" "${out_armel}"
+fi
