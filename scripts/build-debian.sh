837bda1c4d8194719129814edd92a36afeb63c18
Michael Hansen <hansen.mike@gmail.com>
2020-07-07 09:36:32 -0400

    Only use major/minor version in debian package name

--- scripts/build-debian.sh
+++ scripts/build-debian.sh
@@ -14,8 +14,8 @@ shift
 this_dir="$( cd "$( dirname "$0" )" && pwd )"
 src_dir="$(realpath "${this_dir}/..")"
 
-# version="$(cat "${src_dir}/VERSION")"
-version='2.0'
+version="$(cat "${src_dir}/VERSION")"
+out_version='2.0'
 
 # -----------------------------------------------------------------------------
 
@@ -57,25 +57,25 @@ docker buildx build \
 
 # Manually copy out
 in_amd64="${dist_dir}/linux_amd64/voice2json_${version}_amd64.deb"
-out_amd64="${dist_dir}/voice2json_${version}_amd64.deb"
+out_amd64="${dist_dir}/voice2json_${out_version}_amd64.deb"
 if [[ -f "${in_amd64}" ]]; then
     cp "${in_amd64}" "${out_amd64}"
 fi
 
 in_armhf="${dist_dir}/linux_arm_v7/voice2json_${version}_armhf.deb"
-out_armhf="${dist_dir}/voice2json_${version}_armhf.deb"
+out_armhf="${dist_dir}/voice2json_${out_version}_armhf.deb"
 if [[ -f "${in_armhf}" ]]; then
     cp "${in_armhf}" "${out_armhf}"
 fi
 
 in_arm64="${dist_dir}/linux_arm64/voice2json_${version}_arm64.deb"
-out_arm64="${dist_dir}/voice2json_${version}_arm64.deb"
+out_arm64="${dist_dir}/voice2json_${out_version}_arm64.deb"
 if [[ -f "${in_arm64}" ]]; then
     cp "${in_arm64}" "${out_arm64}"
 fi
 
 in_armel="${dist_dir}/linux_arm_v6/voice2json_${version}_armhf.deb"
-out_armel="${dist_dir}/voice2json_${version}_armel.deb"
+out_armel="${dist_dir}/voice2json_${out_version}_armel.deb"
 if [[ -f "${in_armel}" ]]; then
     cp "${in_armel}" "${out_armel}"
 fi
